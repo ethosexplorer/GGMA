32b757331a0dd7e84398c60d092aa139391186c7
Her GitHub Username <her-email@example.com>
2026-05-07 17:00:26 -0500

    chore: cleanup scratch files again

--- scratch/fix_emoji_final.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/App.tsx', 'utf8');
-
-// Replace corrupted emoji
-content = content.replace(/const response = 'dY[^ ]* Let's begin/g, "const response = '🏢 Let\\'s begin");
-
-fs.writeFileSync('src/App.tsx', content);
-console.log('Fixed dY emoji completely!');
